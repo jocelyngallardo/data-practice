918fc8afdfe4ef8aa34d718cdf7597f2a75b4c95
jocelyngallardo <54326467+jocelyngallardo@users.noreply.github.com>
2019-10-22 10:00:53 -0700

    o

--- datapractice.py
+++ datapractice.py
@@ -1,1 +1,7 @@
-
+dict={'Jan':'1', 'Feb':'2', 'Mar':'3', 'Apr':'4', 'May':'5', 'Jun':'6', 'Jul':'7', 'Aug':'8', 'Sep':'9', 'Oct':'10', 'Nov':'11', 'Dec':'12'}
+#print(dict['Dec'])
+for month in dict:
+    if month.startswith('M'):
+        print(month)
+  
+  naomiDict={'name':'Naomi', 'age':16, 'classes'={'0':'The Farm', '1st': 'APUSH', '2nd':'DSW', '3rd':'APES', '4th':'English', '5th':'Math', '6th':'Cross Country'}}
